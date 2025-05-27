c45b090bee6f1dbe3ae6e85bf25d6b5131782f6d
Prince <princewarrior683@gmail.com>
2025-05-27 21:52:08 +0530

    Tuples And Enums Completed ✅

---
+++ enums.ts
@@ -0,0 +1,14 @@
+
+
+// in typescript if we want to create some obj like structure with fixed values we can use enums
+
+// in typescript if we want to make an enum we can use the following syntax
+
+enum StatusCode {
+    OK = 200,
+    NotFound = 404,
+    InternalServerError = 500,
+}
+
+// now if i need to use the enum i can use it like this
+StatusCode.NotFound; // 404
